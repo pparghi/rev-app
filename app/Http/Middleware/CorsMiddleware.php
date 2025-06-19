1c7cc5a803964619dbcf891b54a2674946d0f371
TonySun727 <rsun@baron.finance>
2025-06-19 14:40:10 -0400

    Fix preflight CORS error

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -18,7 +18,11 @@ class CorsMiddleware
     {
         // Handle preflight OPTIONS request
         if ($request->isMethod('OPTIONS')) {
-            $response = response('', 200);
+            $response = response('', 200)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization, X-User-Id')
+            ->header('Access-Control-Max-Age', '86400');
         } else {
             $response = $next($request);
         }
